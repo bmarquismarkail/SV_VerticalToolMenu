67465142f918eabd0b5f6746d0e9fc57d107277f
U-MarquisSeven\Brandon <bmmgreen@gmail.com>
2017-05-29 11:28:34 -0500

    made sorting on Inventory Page ignore the Vertical Tool Menu

--- InventoryPage.cs
+++ InventoryPage.cs
@@ -24,7 +24,7 @@ namespace SB_VerticalToolMenu
             Item heldItem = (Item)fieldInfo.GetValue(this);
             for (int i = Game1.player.maxItems; i < StardewValley.Farmer.maxInventorySpace; i++)
             {
-                if(Game1.player.items[i] != null)
+                if (Game1.player.items[i] != null)
                 {
                     fieldInfo.SetValue(this, Game1.player.items[i]);
                     Game1.player.items[i] = null;
@@ -45,7 +45,7 @@ namespace SB_VerticalToolMenu
                             Game1.playSound("stoneStep");
                             return;
                         }
-                        if(Game1.player.items[Convert.ToInt32(button.name)] != null)
+                        if (Game1.player.items[Convert.ToInt32(button.name)] != null)
                         {
                             Item swapItem = (Item)fieldInfo.GetValue(this);
                             fieldInfo.SetValue(this, Game1.player.items[Convert.ToInt32(button.name)]);
@@ -54,7 +54,7 @@ namespace SB_VerticalToolMenu
 
                         }
                     }
-                    if(Game1.player.items[Convert.ToInt32(button.name)] != null)
+                    if (Game1.player.items[Convert.ToInt32(button.name)] != null)
                     {
                         fieldInfo.SetValue(this, Game1.player.items[Convert.ToInt32(button.name)]);
                         Utility.removeItemFromInventory(Convert.ToInt32(button.name), Game1.player.items);
@@ -62,6 +62,14 @@ namespace SB_VerticalToolMenu
                     }
                 }
             }
+            if (this.organizeButton.containsPoint(x, y))
+            {
+                (Game1.player.items).Sort(0,36, null);
+                (Game1.player.items).Reverse(0, 36);
+                Game1.playSound("Ship");
+                return;
+            }
+
             base.receiveLeftClick(x, y);
         }
 
